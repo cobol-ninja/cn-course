83b0e8e7885ab7da21c70e927412e0482c3c025d
Dov Keshet <dovk@cobol.ninja>
2023-08-12 17:16:33 +0300

    init

--- install-gnucobol/runcd.bat
+++ install-gnucobol/runcd.bat
@@ -1,11 +1,11 @@
 echo off
+rem run cobol dll
 set COB_SYS="C:\gnuCobol"
 set COB_APP="C:\myCobol"
 rem set env
 call C:\gnuCobol\set_env
 rem run
 echo -----------------------------------------------
-echo Running Cobol program %1%
+echo Running Cobol DLL %1%
 echo -----------------------------------------------
-rem call %COB_SYS%\bin\cobcrun.exe C:\myCobol\%1%
-call %COB_APP%\%1%
+call %COB_SYS%\bin\cobcrun.exe %COB_APP%\dll\%1%
